320e574fcbb14b02bba431c4a78d12d623d7b8a9
Jorj-o-jorj <atomlenov@yandex.ru>
2023-08-08 17:56:35 +0300

    homework 1&2

---
+++ Homework_1_seminar/Zadanie_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+
+
+int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA.ToString());
+int numberB = new Random().Next(1,100);
+Console.WriteLine(numberB.ToString());
+
+if(numberA > numberB)
+{
+    Console.WriteLine("max " + (numberA));
+}
+else
+{
+    Console.WriteLine("max " + (numberB));
+}
+
+
+
---
+++ Homework_1_seminar/Zadanie_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = new Random().Next(1,100);
+Console.WriteLine(a.ToString());
+int b = new Random().Next(1,100);
+Console.WriteLine(b.ToString());
+int c = new Random().Next(1,100);
+Console.WriteLine(c.ToString());
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
