ac4bd50b6ea711653d6904813bddeb7b89b76a94
Jorj-o-jorj <atomlenov@yandex.ru>
2023-08-08 16:03:17 +0300

    new Ex_005

--- Example005_Name/Program.cs
+++ Example005_Name/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+if(username=="Борис");
+{
+    Console.WriteLine("Привет, Борис!!! Сколько Лен, Сколько зин!!?!?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
